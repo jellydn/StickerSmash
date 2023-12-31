9d9509bdac7e509fbc6e564cf030506e19fc84ab
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-31 14:18:36 +0800

    feat: add nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Slot } from "expo-router";
 
+import "../global.css";
+
 export default function HomeLayout() {
 	return <Slot />;
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = (api) => {
 	api.cache(true);
 	return {
-		presets: ["babel-preset-expo"],
+		presets: [
+			["babel-preset-expo", { jsxImportSource: "nativewind" }],
+			"nativewind/babel",
+		],
 		plugins: [
 			"@babel/plugin-proposal-export-namespace-from",
 			"react-native-reanimated/plugin",
